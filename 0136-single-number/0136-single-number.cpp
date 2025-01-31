39e0a8fc4c57beb244004cdcad5434494ef90ec2
NISHCHAL GUPTA <98217664+Nishchal1604@users.noreply.github.com>
2025-01-31 21:36:37 +0530

    Time: 0 ms (100%), Space: 20.7 MB (28.52%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int result = 0;
+        for(int num = 0; num<nums.size(); num++){
+            result =result ^ nums[num];
+        }
+        return result;
+        
+    }
+};
